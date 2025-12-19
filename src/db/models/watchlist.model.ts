cc585b7ed56983e5f5890f202c8c82487f1ce645
virtualheet <heetvavadiya456@gmail.com>
2025-12-19 14:05:11 +0530

    Model: @new WatchlistSchema

--- src/db/models/watchlist.model.ts
+++ src/db/models/watchlist.model.ts
@@ -1,9 +1,20 @@
-import { type Document } from "mongoose";
+import { model, Model, models, Schema, type Document } from "mongoose";
 
 
 export interface IWatchlistItem extends Document {
     userId: string;
-    sybol: string;
+    symbol: string;
     company: string;
     addedAt: Date;
 }
+
+const WatchlistSchema = new Schema<IWatchlistItem>({
+    userId: { type: String, required: true },
+    symbol: { type: String, required: true },
+    company: { type: String, required: true },
+    addedAt: { type: Date, default: Date.now },
+});
+
+WatchlistSchema.index({userId: 1, symbol: 1}, {unique: true});
+
+export const WatchList: Model<IWatchlistItem> = (models?.WatchList as Model<IWatchlistItem>) || model<IWatchlistItem>('WatchList', WatchlistSchema);
