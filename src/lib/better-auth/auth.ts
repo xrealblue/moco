9e83e90395d48e1d78140eda82ea2ddfb4d79bc6
virtualheet <heetvavadiya456@gmail.com>
2025-12-19 14:31:29 +0530

    @new lib/better-auth/auth.ts getAuth fn

---
+++ src/lib/better-auth/auth.ts
@@ -0,0 +1,40 @@
+import { connectToDatabase } from "@/db/mongoose";
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+
+let authInstance: ReturnType<typeof betterAuth> | null = null;
+// prevents multiple connections
+
+export const getAuth = async () => {
+    if (!authInstance) {
+        return authInstance;
+    }
+
+    const mongoose = await connectToDatabase();
+
+    const db = mongoose.connection.db;
+
+    if (!db) {
+        throw new Error("Failde to connect Db")
+    }
+
+    authInstance = betterAuth({
+        database: db,
+
+        secret: process.env.BETTER_AUTH_SECRET,
+
+        baseURL: process.env.BETTER_AUTH_URL,
+
+        emailAndPassword: {
+            enabled: true,
+            disableSignUp: false,
+            requireEmailVerification: false,
+            minPasswordLength: 6,
+            maxPasswordLength: 128,
+            autoSignIn: true,
+        },
+        plugin: [nextCookies()],
+    });
+
+    return authInstance;
+}
