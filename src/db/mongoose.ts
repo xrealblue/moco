1476c5ae7c47af48bf9c01d7202daf50764afcb8
virtualheet <heetvavadiya456@gmail.com>
2025-12-19 14:22:09 +0530

    @new connectToDatabase with resueable conn cache with CATCH block

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 declare global {
-     var mongooseCache: {
-        conn: typeof mongoose | null;
-        promise: Promise<typeof mongoose> | null;
-     } | undefined;
+   var mongooseCache: {
+      conn: typeof mongoose | null;
+      promise: Promise<typeof mongoose> | null;
+   } | undefined;
 }
 
 const globalForMongo = globalThis as typeof globalThis & {
@@ -18,7 +18,31 @@ const globalForMongo = globalThis as typeof globalThis & {
 
 let cached = globalForMongo.mongooseCache;
 
-if(!cached) {
-   cached = globalForMongo.mongooseCache = {conn: null, promise: null};
+if (!cached) {
+   cached = globalForMongo.mongooseCache = { conn: null, promise: null };
 } // to not reload in development
 
+export const connectToDatabase = async () => {
+   if (!MONGODB_URI) {
+      throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+   }
+
+   if (cached.conn) {
+      return cached.conn;
+   }
+
+   if (!cached.promise) {
+      cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+   }
+
+   try {
+      cached.conn = await cached.promise;
+   } catch (err) {
+      cached.promise = null;
+      throw err;
+   }
+
+   console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+
+   return cached.conn;
+}
